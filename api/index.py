0655da7d6acee9b16a1c38dd4e81ae86c0fcf42a
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-03-31 16:27:53 +0700

    write file

--- api/index.py
+++ api/index.py
@@ -45,11 +45,6 @@ def generate_frames():
             
         ## read the camera frame
         success,frame=camera.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
