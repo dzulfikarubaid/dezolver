4c2b3f17cece11e63b13293f7962d9b4cb911ea4
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-03-31 16:22:43 +0700

    write file

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, Response
 import requests
 from bs4 import BeautifulSoup
+import cv2
 
 app = Flask(__name__)
 
@@ -36,3 +37,23 @@ def scrape():
     data = str(data)
     
     return render_template("index.html", output=data)
+
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
